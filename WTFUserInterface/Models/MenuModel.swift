25865ecb15b611f78f74c2e0d953de7e4743a796
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-10-12 10:39:33 +0330

    Added menu class model

--- WTFUserInterface/Models/MenuModel.swift
+++ WTFUserInterface/Models/MenuModel.swift
@@ -6,13 +6,19 @@
 //  Copyright © 2019 shndrs. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 final class MenuModel {
     
+    private(set) var image: UIImage!
+    private(set) var action: (() -> Void)!
+    private(set) var description: String!
+    private(set) var title: String!
     
-    
-    
-    
-    
+    init(image: UIImage, action: @escaping () -> Void, description: String, title: String) {
+        self.image = image
+        self.action = action
+        self.description = description
+        self.title = title
+    }
 }
