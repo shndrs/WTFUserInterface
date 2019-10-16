527a01849bda2c7556f895bbc9f68bd04e7f821d
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-10-16 17:00:35 +0330

    Added nibloadble

---
+++ WTFUserInterface/Utils/Extensions/NibLoadable/NibLoadable.swift
@@ -0,0 +1,44 @@
+//
+//  NibLoadable.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 10/16/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+public protocol NibLoadable: class {
+
+    static var nibFile: UINib { get }
+}
+
+// MARK: Default implementation
+
+public extension NibLoadable {
+    
+    static var nibFile: UINib {
+        return UINib(nibName: String(describing: self), bundle: Bundle(for: self))
+    }
+}
+
+// MARK: Support for instantiation from NIB
+
+public extension NibLoadable where Self: UIView {
+  
+    static func loadFromNib() -> Self {
+        guard let view = nibFile.instantiate(withOwner: nil, options: nil).first as? Self else {
+            fatalError("The nib \(nibFile) expected its root view to be of type \(self)")
+        }
+    return view
+  }
+}
+
+protocol ReusableView {}
+
+extension ReusableView where Self: UIView {
+
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
