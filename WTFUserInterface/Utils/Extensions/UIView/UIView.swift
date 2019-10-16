cfcccf9f628250b270e33fa76a25229cde8ae71e
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-10-16 15:48:04 +0330

    Updated UIView extension with loadFromNib method

--- WTFUserInterface/Utils/Extensions/UIView/UIView.swift
+++ WTFUserInterface/Utils/Extensions/UIView/UIView.swift
@@ -10,6 +10,16 @@ import UIKit
 
 extension UIView {
     
+    static func loadFromXib<T>(withOwner: Any? = nil, options: [AnyHashable : Any]? = nil) -> T where T: UIView {
+        let bundle = Bundle(for: self)
+        let nib = UINib(nibName: "\(self)", bundle: bundle)
+
+        guard let view = nib.instantiate(withOwner: withOwner, options: options as? [UINib.OptionsKey : Any]).first as? T else {
+            fatalError("Could not load view from nib file.")
+        }
+        return view
+    }
+    
     func addCornerRadius(radius:CGFloat) {
         self.clipsToBounds = true;
         self.layer.cornerRadius = CGFloat(radius)
