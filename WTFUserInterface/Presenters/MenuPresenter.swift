ee94a79f5a6f663ed4c7308f19b1ae86b9a12a77
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-10-30 10:49:31 +0330

    fixed some hard code issue

--- WTFUserInterface/Presenters/MenuPresenter.swift
+++ WTFUserInterface/Presenters/MenuPresenter.swift
@@ -14,6 +14,9 @@ protocol MenuView: AnyObject {
 
 final class MenuPresenter: NSObject {
     
+    private let price = "32.4 US$"
+    private let title = "Cheese Burger"
+    
     private weak var view: MenuView?
     
     internal init(view: MenuView) {
@@ -23,25 +26,25 @@ final class MenuPresenter: NSObject {
     internal func getData() {
         
         let dwi = DispatchWorkItem {
-            let arrayDet = [ OrderDetails(price: "32.4 US$", title: "Burger"),
-                OrderDetails(price: "32.4 US$", title: "Burger"),
-                OrderDetails(price: "32.4 US$", title: "Burger"),
-                OrderDetails(price: "32.4 US$", title: "Burger"),
-                OrderDetails(price: "32.4 US$", title: "Burger")
+            let arrayDet = [
+                OrderDetails(price: self.price, title: self.title),
+                OrderDetails(price: self.price, title: self.title),
+                OrderDetails(price: self.price, title: self.title),
+                OrderDetails(price: self.price, title: self.title),
+                OrderDetails(price: self.price, title: self.title)
             ]
             
             var array = Array<MenuModel>()
             let item0 = MenuModel(action: {
-                
-            }, price: "162 US $", title: "Burger", details: arrayDet)
+                print(self.title)
+            }, price: "162 US $", title: self.title, details: arrayDet)
             
             let item1 = MenuModel(action: {
-                
-            }, price: "162 US $", title: "Burger", details: arrayDet)
+                print(self.title)
+            }, price: "162 US $", title: self.title, details: arrayDet)
             
             array.append(item0)
             array.append(item1)
-            
             
             DispatchQueue.main.async { [weak self] in
                 self?.view?.setTableView(with: array)
