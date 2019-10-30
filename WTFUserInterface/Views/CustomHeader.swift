6490b6c84b992e462f29a8a6eab617069237b7d7
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-10-30 11:58:21 +0330

    Updated with expanedButton outlet

--- WTFUserInterface/Views/CustomHeader.swift
+++ WTFUserInterface/Views/CustomHeader.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 public protocol ExpanedCellDelegate: AnyObject {
+    
     func expanedButtonPressed(section: Int) -> Void
 }
 
@@ -23,40 +24,43 @@ final class CustomHeader: UITableViewCell, NibLoadable, ReusableView {
             expandButton.addBorder(color: .white, thickness: 0.6)
         }
     }
-    
     @IBOutlet private weak var backView: UIView! {
         didSet {
             backView.addCornerRadius(radius: 5)
             backView.addBorder(color: .darkGray, thickness: 0.5)
         }
     }
-    
     @IBOutlet private weak var detailsView: UIView! {
         didSet {
             detailsView.addCornerRadius(radius: 5)
             detailsView.addBorder(color: .darkGray, thickness: 0.5)
         }
     }
-    
     @IBOutlet private weak var addressView: UIView! {
         didSet {
             addressView.addCornerRadius(radius: 5)
             addressView.addBorder(color: .darkGray, thickness: 0.5)
         }
     }
-    
     @IBOutlet private weak var amountView: UIView! {
         didSet {
             amountView.addCornerRadius(radius: 5)
             amountView.addBorder(color: .white, thickness: 0.5)
         }
     }
+    @IBOutlet private weak var expanedButton: UIButton!
+}
+
+// MARK: - Methods
+
+extension CustomHeader {
     
     @IBAction private func expanedButtonPressed(_ sender: UIButton) {
+        
         delegate?.expanedButtonPressed(section: self.section)
     }
     
-    public func fill(header by: MenuModel) {
+    internal func fill(header by: MenuModel) {
         
     }
 }
