940d6f26ba27adce2b865f6d4428f43170ea439b
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-11-12 16:45:57 +0330

    Added RightCatHeader

--- WTFUserInterface/Modules/Category/View/Cells/RightCatHeader.swift
+++ WTFUserInterface/Modules/Category/View/Cells/RightCatHeader.swift
@@ -8,17 +8,11 @@
 
 import UIKit
 
-class RightCatHeader: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+final class RightCatHeader: CategoryHeader, ReusableView {
+    override var backView: UIView! {
+        didSet {
+            backView.customCatHeaderRight()
+        }
     }
     
 }
