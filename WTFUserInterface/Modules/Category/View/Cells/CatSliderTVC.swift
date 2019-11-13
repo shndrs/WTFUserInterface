2d48cf9620c86f728bb95c6b818d38b867f08f6b
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-11-13 14:50:50 +0330

    Added CatSliderTVC

---
+++ WTFUserInterface/Modules/Category/View/Cells/CatSliderTVC.swift
@@ -0,0 +1,13 @@
+//
+//  CatSliderTVC.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 11/13/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+final class CatSliderTVC: UITableViewCell, ReusableView {
+
+}
