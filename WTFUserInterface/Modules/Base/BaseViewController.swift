e86c3341348b78b40f361cabe47d1ae2495dbac5
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-11-18 11:19:35 +0330

    Added BaseViewController

---
+++ WTFUserInterface/Modules/Base/BaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseViewController.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 11/18/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+}
+
+// MARK: - Life Cycle
+
+extension BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
