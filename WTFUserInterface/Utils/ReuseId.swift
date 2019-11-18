e24ac9deeaf0f6fbe50a7a7a7c16adb0c5e14435
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-11-18 15:41:55 +0330

    Added ReuseId struct

---
+++ WTFUserInterface/Utils/ReuseId.swift
@@ -0,0 +1,26 @@
+//
+//  ReuseId.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 11/18/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import Foundation
+
+struct RIDs {
+    
+    static let categoryVC = [
+        LeftCatHeader.reuseIdentifier,
+        RightCatHeader.reuseIdentifier,
+        CategoryTVC.reuseIdentifier,
+        CatSliderTVC.reuseIdentifier
+    ]
+    
+    static let menuVC = [
+        MenuTVC.reuseIdentifier,
+        CustomHeader.reuseIdentifier
+    ]
+    
+    
+}
