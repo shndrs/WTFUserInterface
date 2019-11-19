0efd24a37291489459dd9060795d5390aa136c1e
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-11-19 11:36:59 +0330

    Added Typealias file

---
+++ WTFUserInterface/Utils/Typealias.swift
@@ -0,0 +1,12 @@
+//
+//  Typealias.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 11/19/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import Foundation
+
+typealias Action = (() -> Void)
+
