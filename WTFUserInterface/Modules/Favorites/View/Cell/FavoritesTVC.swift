37c2ef86a93a403172ce6bd34a775aba497b3bcf
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-11-19 16:29:26 +0330

    Added FavoritesTVC

---
+++ WTFUserInterface/Modules/Favorites/View/Cell/FavoritesTVC.swift
@@ -0,0 +1,13 @@
+//
+//  FavoritesTVC.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 11/19/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+final class FavoritesTVC: UITableViewCell, ReusableView {
+    
+}
