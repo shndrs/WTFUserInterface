a71565dfc657890f9033a0693e078e9d855b6a9f
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-12-02 15:58:34 +0330

    Added ProductsTVC

---
+++ WTFUserInterface/Modules/Products/View/Cells/ProductsTVC.swift
@@ -0,0 +1,15 @@
+//
+//  ProductsTVC.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 12/2/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+final class ProductsTVC: UITableViewCell, ReusableView {
+    
+    
+    
+}
