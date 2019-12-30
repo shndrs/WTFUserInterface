2bd56d3ffed31907debaba814b6501164248f927
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-12-30 10:42:41 +0330

    Added Colors struct

---
+++ WTFUserInterface/Utils/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 12/30/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    
+    public static let darkGray = UIColor(red: 85.0/255.0,
+                                         green: 85.0/255.0,
+                                         blue: 85.0/255.0,
+                                         alpha: 1.0)
+    
+    
+}
