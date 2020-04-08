46f9b53d043a9dd7490fa723c9ef10e921ea3def
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-04-08 11:02:52 +0430

    Add login model

---
+++ WTFUserInterface/Modules/Login/Model/Login.swift
@@ -0,0 +1,19 @@
+//
+//  Login.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 4/8/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+import Foundation
+
+struct Login {
+    var username: String!
+    var password: String!
+    
+    init(username: String, password: String) {
+        self.username = username
+        self.password = password
+    }
+}
