8cf740d7456348b2e0df17d1c04dd343ac735c37
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-05-04 11:36:30 +0430

    Added UIViewController extensions file

---
+++ WTFUserInterface/Utils/Extensions/UIViewController/UIViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UIViewController.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 5/4/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    public class func instantiate(from storyboard: UIStoryboard? = nil) -> Self {
+        return get(storyboard:storyboard)
+    }
+    
+    public class func instantiate(withStoryboardName: String) -> Self {
+        return instantiate(from: UIStoryboard(name: withStoryboardName, bundle: nil))
+    }
+       
+    @discardableResult
+    private class func get<T : UIViewController>(storyboard: UIStoryboard? = nil) -> T {
+        let className = String(describing: self)
+        var story = storyboard
+        if story == nil {
+            story = UIStoryboard(name: className, bundle: .main)
+        }
+        return story!.instantiateViewController(withIdentifier: className) as! T
+    }
+}
