833ffeaa6ab05508be495332b68cfc82e7ae49d6
shndrs <sahandraeisi1994@gmail.com>
2020-05-18 07:58:09 +0430

    Added Defaults storage

---
+++ WTFUserInterface/Utils/Storage/Defaults.swift
@@ -0,0 +1,28 @@
+//
+//  Defaults.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 5/18/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+import Foundation
+
+final class Defaults {
+
+    private let defaults: UserDefaults
+
+    init(defaults: UserDefaults = .standard) {
+        self.defaults = defaults
+    }
+
+    subscript(key: String) -> String? {
+        get {
+            return defaults.string(forKey: key)
+        }
+
+        set {
+            defaults.set(newValue, forKey: key)
+        }
+    }
+}
