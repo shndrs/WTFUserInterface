a2aca6243dc0e8e23d289b82df395782768c4d62
shndrs <sahandraeisi1994@gmail.com>
2020-05-18 08:19:46 +0430

    Added Register entity

---
+++ WTFUserInterface/Modules/Register/Entity/Register.swift
@@ -0,0 +1,18 @@
+//
+//  Register.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 5/18/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+import Foundation
+
+struct Register {
+    
+    public var username: String!
+    public var password: String!
+    public var repeatPassword: String!
+    public var email: String!
+    
+}
