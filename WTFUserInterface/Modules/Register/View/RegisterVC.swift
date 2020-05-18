b082643dd98aab34cd65fc353c422f62b8157674
shndrs <sahandraeisi1994@gmail.com>
2020-05-18 08:05:24 +0430

    Added RegisterVC

---
+++ WTFUserInterface/Modules/Register/View/RegisterVC.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterVC.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 5/18/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+import UIKit
+
+final class RegisterVC: BaseViewController {
+    
+}
+
+// MARK: - Methods
+
+extension RegisterVC {
+    @IBAction private func registerButtonPressed(_ sender: Any) {
+    }
+}
+
+// MARK: - Life Cycle
+
+extension RegisterVC {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
