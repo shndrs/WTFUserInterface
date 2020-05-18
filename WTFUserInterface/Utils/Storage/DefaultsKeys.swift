2b0d08b7e8773cc8c35b76ad08c46c7f8b6509a0
shndrs <sahandraeisi1994@gmail.com>
2020-05-18 08:00:43 +0430

    Added DefaultsKeys

---
+++ WTFUserInterface/Utils/Storage/DefaultsKeys.swift
@@ -0,0 +1,16 @@
+//
+//  DefaultsKeys.swift
+//  WTFUserInterface
+//
+//  Created by NP2 on 5/18/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+import Foundation
+
+enum DefaultsKeys: String {
+    case username
+    case password
+    case email
+    case hint
+}
