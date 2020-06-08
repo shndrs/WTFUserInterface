c2db3f273c7c2b8e91d509947a67e428730bf0a1
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-06-08 11:34:57 +0430

    Updated register with _specialize

--- WTFUserInterface/Utils/Register.swift
+++ WTFUserInterface/Utils/Register.swift
@@ -23,7 +23,7 @@ extension Register {
        ///   - id: The name of the nib (use reuseIdentifier if you inherit from ReusableView protocol)
        /// - Note: Component should be sub class of UITableView or UICollectionView
        ///         otherwise it will break IN YOUR FACE!!! :D
-    
+    @_specialize(where T == (UITableView, UICollectionView))
     public static func `in`<T>(component: T, id: String) {
         
         let nib = UINib(nibName: id, bundle: nil)
@@ -31,15 +31,11 @@ extension Register {
         switch component {
             
         case is UITableView:
-            
             guard let cell = component as? UITableView else { return }
             cell.register(nib, forCellReuseIdentifier: id)
-        
         case is UICollectionView:
-            
             guard let cell = component as? UICollectionView else { return }
             cell.register(nib, forCellWithReuseIdentifier: id)
-        
         default:
             break;
         }
